398d4e12fb560cb8efab59a2835ab6b5bac3526a
mitchya1 <mitchell.anderson@colorado.edu>
2019-10-28 12:32:57 -0600

    updates

--- contacts.go
+++ contacts.go
@@ -17,10 +17,22 @@ type ZohoContactsAllResponse struct {
 	} `json:"data"`
 }
 
+type ZohoContactsCountResponse struct {
+	Count string `json:"count"`
+}
+
+type ZohoContactsViewsResponse struct {
+	Data []struct {
+		Custom   bool   `json:"isCustomView"`
+		ViewName string `json:"name"`
+		ID       string `json:"id"`
+	} `json:"data"`
+}
+
 // GetAllContacts returns a ZohoContactsAll struct
 func (h *ZohoHeaders) GetAllContacts() ZohoContactsAllResponse {
 
-	url := fmt.Sprintf("%s/contacts", ZohoBaseURL)
+	url := fmt.Sprintf("%s/contacts?limit=99", ZohoBaseURL)
 
 	tokenHeaderString := fmt.Sprintf("Zoho-authtoken %s", h.Token)
 
@@ -34,8 +46,107 @@ func (h *ZohoHeaders) GetAllContacts() ZohoContactsAllResponse {
 	}
 
 	// Limit defaults to 10
-	q := req.URL.Query()
-	q.Add("limit", "100")
+	//q := req.URL.Query()
+	//q.Add("limit", "15")
+
+	req.Header.Set("orgId", h.OrgID)
+	req.Header.Set("Authorization", tokenHeaderString)
+
+	resp, err := c.Do(req)
+
+	if err != nil {
+		fmt.Println("Error making request to Zoho API to GetAllContacts")
+		ZohoErrHandler(err)
+	}
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(responseBody))
+
+	if err != nil {
+		fmt.Println("Error reading GetAllContacts response")
+		fmt.Println("Dumping response body for debugging")
+		fmt.Println(string(responseBody))
+		ZohoErrHandler(err)
+	}
+
+	r := ZohoContactsAllResponse{}
+
+	err = json.Unmarshal(responseBody, &r)
+
+	if err != nil {
+		fmt.Println("Error unmarshalling GetAllContacts JSON")
+		fmt.Println("Dumping response body for debugging")
+		fmt.Println(string(responseBody))
+		ZohoErrHandler(err)
+	}
+
+	return r
+}
+
+func (h *ZohoHeaders) ListContactViews() ZohoContactsViewsResponse {
+	// For some reason, adding the query with URL.Query() isn't working
+	url := fmt.Sprintf("%s/views?module=contacts", ZohoBaseURL)
+
+	tokenHeaderString := fmt.Sprintf("Zoho-authtoken %s", h.Token)
+
+	c := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		fmt.Println("Error creating HTTP request to GetAllTickets")
+		ZohoErrHandler(err)
+	}
+
+	req.Header.Set("orgId", h.OrgID)
+	req.Header.Set("Authorization", tokenHeaderString)
+
+	//q := req.URL.Query()
+	//q.Add("module", "Contacts")
+
+	resp, err := c.Do(req)
+
+	if err != nil {
+		fmt.Println("Error making request to Zoho API to GetAllContacts")
+		ZohoErrHandler(err)
+	}
+
+	responseBody, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Println("Error reading GetAllContacts response")
+		ZohoErrHandler(err)
+	}
+
+	r := ZohoContactsViewsResponse{}
+
+	err = json.Unmarshal(responseBody, &r)
+
+	if err != nil {
+		fmt.Println("Error unmarshalling ListContactViews JSON")
+		fmt.Println("Dumping response body for debugging")
+		fmt.Println(string(responseBody))
+		ZohoErrHandler(err)
+	}
+
+	return r
+
+}
+
+func (h *ZohoHeaders) GetContactCount(v string) ZohoContactsCountResponse {
+
+	url := fmt.Sprintf("%s/contacts/count?viewId=%s", ZohoBaseURL, v)
+
+	tokenHeaderString := fmt.Sprintf("Zoho-authtoken %s", h.Token)
+
+	c := &http.Client{}
+
+	req, err := http.NewRequest("GET", url, nil)
+
+	if err != nil {
+		fmt.Println("Error creating HTTP request to GetAllTickets")
+		ZohoErrHandler(err)
+	}
 
 	req.Header.Set("orgId", h.OrgID)
 	req.Header.Set("Authorization", tokenHeaderString)
@@ -54,15 +165,16 @@ func (h *ZohoHeaders) GetAllContacts() ZohoContactsAllResponse {
 		ZohoErrHandler(err)
 	}
 
-	r := ZohoContactsAllResponse{}
+	r := ZohoContactsCountResponse{}
 
 	err = json.Unmarshal(responseBody, &r)
 
 	if err != nil {
 		fmt.Println("Error unmarshalling GetAllContacts JSON")
+		fmt.Println("Dumping response body for debugging")
+		fmt.Println(string(responseBody))
 		ZohoErrHandler(err)
 	}
 
 	return r
-
 }
